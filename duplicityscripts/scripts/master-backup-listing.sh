2cc50c0f72dfa97a32feefe438c7c46e128b5af6
Yvan GODARD <godardyvan@gmail.com>
2016-03-30 07:14:01 +0200

    commit

--- duplicityscripts/scripts/master-backup-listing.sh
+++ duplicityscripts/scripts/master-backup-listing.sh
@@ -16,39 +16,42 @@ fi
 
 export PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin
 
-: ${BASE:=/var/backups/master}
 : ${DEV:=}
 : ${TAG:=Master-Backup}
+: ${BASE:=/var/backups}
 : ${URL:=}
 : ${WHAT:=}
 : ${OPTIONS:=}
 : ${PASSPHRASE:=__duplicity__GnuPG__passphrase__}
 : ${CACHE:=~/.cache/duplicity}
 
+BASE=${BASE%/}/${TAG%/}
+BASE_NAGIOSTEST=${BASE}/NagiosTest
+
 # Duplicity sert pour les sauvegardes complètes et incrémentales vers une destination
-if [ -z "$URL" ]; then
-	echo "Pas d'URL définie pour lister la sauvegarde"
+if [ -z "${URL}" ]; then
+	echo "Pas d'URL définie pour lister la sauvegarde."
 	exit 1
 fi
 
-[ -d $BASE ] || mkdir -p $BASE
-[ -n "$DEV" ] && mount $DEV $BASE >/dev/null 2>&1
-pushd $BASE >/dev/null 2>&1 || exit 1
-[ -d $BASE/tmp ] || mkdir -p $BASE/tmp
+[ -d ${BASE} ] || mkdir -p ${BASE}
+[ -n "${DEV}" ] && mount ${DEV} ${BASE} >/dev/null 2>&1
+pushd ${BASE} >/dev/null 2>&1 || exit 1
+[ -d ${BASE}/tmp ] || mkdir -p ${BASE}/tmp
 
 DUPLICITY_OPTS="$*"
 
 # Dossier temporaire à utiliser
-DUPLICITY_OPTS="$DUPLICITY_OPTS --tempdir $BASE/tmp"
+DUPLICITY_OPTS="${DUPLICITY_OPTS} --tempdir ${BASE}/tmp"
 
 unset CACHE_OPTS
-if [ -n "$CACHE" ]; then
-	[ -d "$CACHE" ] || mkdir -p "$CACHE"
-	CACHE_OPTS="--archive-dir $CACHE"
+if [ -n "${CACHE}" ]; then
+	[ -d "${CACHE}" ] || mkdir -p "${CACHE}"
+	CACHE_OPTS="--archive-dir ${CACHE}"
 fi
 
-if [ -n "$NAME" ]; then
-	CACHE_OPTS="$CACHE_OPTS --name $NAME"
+if [ -n "${NAME}" ]; then
+	CACHE_OPTS="${CACHE_OPTS} --name ${NAME}"
 fi
 
 H=$(hostname -s)
@@ -65,14 +68,14 @@ export CLOUDFILES_USERNAME=${HUBICUSER}
 export CLOUDFILES_APIKEY=${HUBICPASSWORD}
 export CLOUDFILES_AUTHURL="hubic|${HUBICAPPID}|${HUBICAPPSECRET}|${HUBICAPPURLREDIRECT}"
 
-duplicity list-current-files $CACHE_OPTS $DUPLICITY_OPTS ${URL}
+duplicity list-current-files ${CACHE_OPTS} ${DUPLICITY_OPTS} ${URL}
 ERR=$?
 
 popd >/dev/null 2>&1
-[ -n "$DEV" ] && umount $BASE >/dev/null 2>&1
+[ -n "${DEV}" ] && umount ${BASE} >/dev/null 2>&1
 
 unset CLOUDFILES_USERNAME
 unset CLOUDFILES_APIKEY
 unset CLOUDFILES_AUTHURL
 
-exit $ERR
+exit ${ERR}
